ff80d6aeacd43c29e864e2d2b85de553c1391ddd
Your Name <tolkun.kolya@mail.ru>
2021-11-18 19:45:52 +0300

    fix: change Card, Categories, MainPage

--- src/Categories/Categories.tsx
+++ src/Categories/Categories.tsx
@@ -1,16 +1,12 @@
 import { Card } from "../Card";
 
 export const Categories = ({ goodCategories }) => {
-  const categoriesMap = [goodCategories]
   return (
     <div>
-      {categoriesMap.map( (item) => {
-        return(
-          <h3>item.Categories[0].label</h3>
-          <div>item.items.map( (el) => <Card card={el} /> )</div>
-        )
-      })}
-      <Card />
+      {goodCategories.category.label}
+      {goodCategories.items.map((item) => (
+        <Card label={item.label} price={item.price} img={item.img} />
+      ))}
     </div>
   );
 };
