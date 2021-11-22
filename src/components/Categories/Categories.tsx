098f316b665c2ee6bb1711af4d087aeb7318d5c7
Your Name <tolkun.kolya@mail.ru>
2021-11-22 17:19:38 +0300

    fix: change Categories component, add antd functional

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,11 +1,20 @@
-import { Card } from "../Card";
+import { CardProducts } from "../Card";
+import { useSelector } from "react-redux";
+import { Selectors } from "../../store";
 
-export const Categories = ({ goodCategories }) => {
+interface popularCategoriesMap {
+  label: string,
+  price: number,
+  img: string
+}
+
+export const Categories = () => {
+  const popularCategories = useSelector(Selectors.getPopularCategories);
   return (
     <div>
-      {goodCategories.category.label}
-      {goodCategories.items.map((item) => (
-        <Card label={item.label} price={item.price} img={item.img} />
+      {popularCategories.category.label}
+      {popularCategories.items.map((item: popularCategoriesMap) => (
+        <CardProducts label={item.label} price={item.price} img={item.img} />
       ))}
     </div>
   );
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -2,7 +2,7 @@ import { createStore } from 'redux'
 
 
 const INITIAL_STORE = {
-    popularCategories: [{
+    popularCategories: {
         category: { id: 1, type: "house", label: "Дом" },
         items: [
             {
@@ -11,64 +11,55 @@ const INITIAL_STORE = {
                 label: "Коврик",
                 price: 88,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 2,
                 category_type: "house",
                 label: "Коврик",
                 price: 92,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 3,
                 category_type: "house",
                 label: "Коврик",
                 price: 98,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 4,
                 category_type: "house",
                 label: "Коврик",
                 price: 96,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 5,
                 category_type: "house",
                 label: "Коврик",
                 price: 99,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 6,
                 category_type: "house",
                 label: "Коврик",
                 price: 99,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 7,
                 category_type: "house",
                 label: "Коврик",
                 price: 99,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 8,
                 category_type: "house",
                 label: "Коврик",
                 price: 99,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 9,
                 category_type: "house",
                 label: "Коврик",
                 price: 99,
                 img: "https://source.unsplash.com/random"
-            },
-            {
+            }, {
                 id: 10,
                 category_type: "house",
                 label: "Коврик",
@@ -99,7 +90,7 @@ const INITIAL_STORE = {
                 price: 99,
                 img: "https://source.unsplash.com/random"
             }]
-    }], 
+    }, 
     categories: [
     { id: 1, type: "house", label: "Дом" },
     { id: 2, type: "house2", label: "Дом2" },
