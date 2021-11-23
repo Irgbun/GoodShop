b739130d72a5e8d77e5ddba7ff0cb44e997bee54
Your Name <tolkun.kolya@mail.ru>
2021-11-23 16:20:26 +0300

    feat: change App component for CategoriesPage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import { MainPage } from './MainPage'
+import { CategoriesPage } from './CategoriesPage'
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={ <MainPage /> } />
+      <Route path="/:type" element={ <CategoriesPage /> } />
     </Routes>
   );
 }
