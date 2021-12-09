ba98a2d4e921380026920225cb2ae87c29f8619f
Your Name <tolkun.kolya@mail.ru>
2021-12-09 19:11:54 +0300

    fix: change Menu component

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,24 +5,26 @@ import { Link } from "react-router-dom";
 import { Menu, Row, Col } from "antd";
 
 interface categpriesMap {
-  id: number,
+  id: string,
   type: string,
   label: string
 }
 
 export const MenuCategories = () => {
-  const categories = useSelector(MenuCategoriesSelectors.getCategories);
   const dispatch = useDispatch()
 
   useEffect(() => {
     dispatch(MenuCategoriesActions.fetchMenuCategories())
   }, [])
 
+  const categories = useSelector(MenuCategoriesSelectors.getCategories);
   return (
     <Row>
       <Col span={6}>
         <Menu mode="vertical">
-          {categories.loadStatus && categories.data.map((item: categpriesMap) => {
+          {categories.loadStatus === 'failure' && 'Something went wrong'}
+          {categories.loadStatus === 'loading' && 'Loading list'}
+          {categories.loadStatus === 'loaded' && categories.data.map((item: categpriesMap) => {
             return (
               <Menu.Item key={item.id}>
                 <Link to={`${item.type}`}> {item.label} </Link>
