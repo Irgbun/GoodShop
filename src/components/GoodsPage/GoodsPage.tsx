b18329c25553c42996d4bf4712580c69410db60b
Your Name <tolkun.kolya@mail.ru>
2021-12-22 00:19:19 +0300

    fix: change GoodsPage component

--- src/components/GoodsPage/GoodsPage.tsx
+++ src/components/GoodsPage/GoodsPage.tsx
@@ -3,14 +3,32 @@ import { Table, Select, Slider, Input, Pagination } from 'antd'
 import { useSelector, useDispatch } from "react-redux";
 import { GoodsSelectors, GoodsActions, CategoriesSelectors, CategoriesActions } from "../../store";
 import { useEffect, useState, useCallback } from 'react';
-import { debounce } from 'lodash'
+import { debounce, values } from 'lodash'
 import { columnsName } from './constants'
 import { useNavigate } from 'react-router-dom';
 import { Loader } from '../Loader'
 
+interface State {
+    text?: string,
+    minPrice?: number,
+    maxPrice?: number,
+    category?: string[],
+    limit?: number,
+    offset?: number
+}
 
+interface DebounceFunction {
+    text?: string,
+    minPrice?: number,
+    maxPrice?: number,
+    category?: string[],
+    limit?: number,
+    offset?: number
+}
 
 export const GoodsPage = () => {
+
+    const [ vaLue, setVaLue ] = useState<State>({ text: "", minPrice: 0, maxPrice: 1000, category: [], limit: 20, offset: 220 })
 
     const [ inputValue, setInputValue ] = useState("")
     const [ sliderValue, setSliderValue ] = useState<number[]>([])
@@ -24,16 +42,19 @@ export const GoodsPage = () => {
 
     const optionCategories: any = []
 
+    const debounceDispatch = useCallback(debounce(( {text, minPrice, maxPrice, category, limit, offset}: DebounceFunction) => {
+        dispatch(GoodsActions.fetchGoods({type: category, text, minPrice, maxPrice, limit, offset}))
+    }, 1500), [])
+
     useEffect(() => {
-        dispatch(GoodsActions.fetchGoods({ limit: 20, offset: 220 }))
         dispatch(CategoriesActions.fetchCategories({}))
-    }, [])
+        debounceDispatch(vaLue)
+    }, [vaLue])
 
     const navigate = useNavigate()
 
     const goods = useSelector(GoodsSelectors.getGoodsWithCategory);
     const goodsStatus = useSelector(GoodsSelectors.getGoodsStatus)
-    const total = useSelector(GoodsSelectors.getGoodsTotal)
     const categories = useSelector(CategoriesSelectors.getCategories)
 
     categories.map((item) => {
@@ -42,36 +63,37 @@ export const GoodsPage = () => {
 
     const inputOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newValue = event.target.value
-        setInputValue(newValue)
+        setVaLue({ text: newValue })
     }
 
-    const debounceDispatch = useCallback(debounce(( text, minPrice, maxPrice, type, limit, offset?) => {
-        dispatch(GoodsActions.fetchGoods({type, text, minPrice, maxPrice}))
-    }, 1500), [])
-
-    useEffect(() => {
-        debounceDispatch(inputValue, sliderValue[0], sliderValue[1], selectValue, paginationPageSize, total)
-    }, [inputValue, sliderValue, selectValue, paginationPageSize, total])
-
     const sliderOnChange = (value: [number, number]) => {
-        setSliderValue(value)
+        setVaLue({ minPrice: value[0], maxPrice: value[1] })
     }
 
     const selectOnChange = (value: string[]) => {
-        setSelectValue(value)
+        setVaLue({ category: value })
     }
 
-    const paginationOnChange = (page: number, pageSize: number) => {
-        setPaginationPageSize(pageSize)
+    const paginationOnChange = (page: number, pageSize?: number) => {
+        if(pageSize !== undefined) {
+            setVaLue({ limit: pageSize })
+        }
     }
 
     return (
         <div>
             <div>
-                <Input value={inputValue} style={{ width: '200px' }} placeholder='Поиск по товарам' onChange={inputOnChange} />
+                <Input value={vaLue.text} style={{ width: '200px' }} placeholder='Поиск по товарам' onChange={inputOnChange} />
                 <div>
                     Цена:
-                    <Slider range defaultValue={[0, 1000]} max={1000} style={{ width: '200px' }} onChange={sliderOnChange} />
+                    <Slider 
+                    range 
+                    defaultValue={[0, 1000]} 
+                    min={vaLue.minPrice} 
+                    max={vaLue.maxPrice} 
+                    style={{ width: '200px' }} 
+                    onChange={sliderOnChange} 
+                    />
                 </div>
                 <Select
                 mode="multiple"
@@ -91,10 +113,10 @@ export const GoodsPage = () => {
                     columns={columnsName} 
                     pagination={{
                         defaultCurrent: 1,
-                        defaultPageSize: paginationPageSize, 
-                        pageSize: 20,
+                        defaultPageSize: vaLue.limit, 
+                        pageSize: vaLue.limit,
                         pageSizeOptions: ['10', '20', '60', '100'],
-                        total: paginationTotal,
+                        total: vaLue.offset,
                         onChange: paginationOnChange
                     }}
                     onRow={(record) => {
