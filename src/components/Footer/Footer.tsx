2734aa3cb36c7801ab0ab1835473dae0efdec09d
Your Name <tolkun.kolya@mail.ru>
2021-12-22 03:10:43 +0300

    fix: change Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,9 +1,17 @@
-
+import css from './Footer.module.css'
 
 export const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={css.FooterWrapper}>
+            <div>
+                This project create by Nickolay Tolkun.   
+            </div>
+            <div>
+                Contacts with telefon: +375 44 859-56-48, +375 25 442-64-25    
+            </div>
+            <div>
+                Contacts in social media: vk.com facebook.com
+            </div>
         </div>
     )
 }
